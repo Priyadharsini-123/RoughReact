6282961b30bf316265b458e999846bf499259077
Priyadharsini-123 <priyadharsinib444@gmail.com>
2024-04-15 22:28:09 +0530

    some changes

--- rough/src/pages/Trainseat.js
+++ rough/src/pages/Trainseat.js
@@ -3,6 +3,7 @@ import { Card, Container } from "react-bootstrap";
 import Rupee from "../assets/images/Indian Rs.png";
 import Seat from "../assets/images/Train Seat.png";
 import Train from "../assets/images/Train Station.png";
+import { Button } from "react-bootstrap";
 const Trainseat = () => {
   return (
     <>
@@ -81,48 +82,90 @@ const Trainseat = () => {
 
           {/* <===================From start================> */}
           <div className="container">
-            <div className="row">
-              {/* First column */}
-              <div className="col-md-6" style={{}}>
-                <div className="row">
-                  <div className="col-md-6" style={{}}>
-                    <div className="From">
-                      <p className="FromLabel">From</p>
-                      <p className="Source">Enter Source Name</p>
-                    </div>
-                  </div>
-                  <div className="col-md-6" style={{}}>
-                    <div className="to">
-                      <p className="Seatfrom">To</p>
-                      <p className="destination">Enter Destination Name</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Second column */}
-              <div className="col-md-3" style={{}}>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div className="Seatcard-inner-left">
-                      <span className="DownArrow fswDownArrow"></span>
-                      <p className="Seatfrom">Departure</p>
-                      <p className="destination" style={{ color: "#141823" }}>
-                        09 Apr'24
-                      </p>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div className="Seatcard-inner-left">
-                      <span className="DownArrow fswDownArrow"></span>
-                      <p className="Seatfrom">Departure</p>
-                      <p className="destination" style={{ color: "#141823" }}>
-                        09 Apr'24
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="row">
+  {/* First column */}
+  <div className="col-md-6 position-relative"> {/* Added position-relative */}
+    <div className="row">
+      <div className="col-md-6">
+        <div className="From">
+          <p className="FromLabel">From</p>
+          <p className="Source">Enter Source Name</p>
+        </div>
+        {/* Adjusted the position of the image */}
+        <span style={{
+          boxShadow: '0 0 10px rgba(0,0,0,.103)',
+          borderRadius: '12px',
+          backgroundColor: '#fff',
+          width: '38px',
+          height: '38px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+          position: 'absolute',
+          left: '267px',
+          top: '18px',
+          zIndex: 1,
+          padding: '8px'
+        }}>
+          <img src="https://gos3.ibcdn.com/reverse-arrow-1668596930.png" alt="arrow icon" width="20" height="20" />
+        </span>
+      </div>
+      <div className="col-md-6">
+        <div className="to">
+          <p className="Seatfrom">To</p>
+          <p className="destination">Enter Destination Name</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* Second column */}
+  <div className="col-md-3">
+    <div class="row">
+      <div class="col-md-6">
+        <div className="Seatcard-inner-left">
+          <span className="DownArrow fswDownArrow"></span>
+          <p className="Seatfrom">Date</p>
+          <p className="destination" style={{ color: "#141823" }}>
+            09 Apr'24
+          </p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div className="Seatcard-inner-left">
+          <span className="DownArrow fswDownArrow"></span>
+          <p className="Seatfrom">Quota</p>
+          <p className="destination" style={{ color: "#141823" }}>
+            General
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* <div className="col-md-3">
+    <div class="row">
+      <div class="col-md-4">
+        <Button  style={{ border:'2px solid black' }}>
+         Today
+        </Button>
+      </div>
+      <div class="col-md-4">
+        <Button style={{ border:'2px solid black' }}>
+        Tomorrow
+        </Button>
+      </div>
+      <div class="col-md-4">
+        <Button style={{ border:'2px solid black' }}>
+         Day after Tomorrow
+        </Button>
+      </div>
+    </div>
+  </div> */}
+  <div className="Seatbutton">
+    <span className="Seatsearch" style={{ marginTop: '60px' }}>SEARCH SEATS</span>
+  </div>
+</div>
+
           </div>
         </Card>
       </div>
